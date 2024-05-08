2e96b013c5ffc69be92b708390a08481214b3d0e
nataliagarciasanchez <natalia.garciasanchez@usp.ceu.es>
2024-05-08 19:39:02 +0200

    JDBC Manager

--- FertilityClinic/src/FertilityClinicJDBC/JDBCManager.java
+++ FertilityClinic/src/FertilityClinicJDBC/JDBCManager.java
@@ -1,12 +1,37 @@
 package FertilityClinicJDBC;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JDBCManager {
 	
 	private Connection c = null; 
 	
 	public JDBCManager() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+		    c = DriverManager.getConnection("jdbc:sqlite:./db/DDBB_Grupo9.db");
+		    c.createStatement().execute("PRAGMA foreign_keys=ON");
+		    
+		    System.out.println("Database connection opened.");
+		    
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			System.out.println("Libraries not loaded.");
+		}
+	}
+	
+	public Connection getConnection(){
+		return c; 
+	}
+	public void disconnect() {
+		try {
+			c.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
