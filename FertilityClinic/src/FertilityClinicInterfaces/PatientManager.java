88959d38c8b510f3fcdc973995451854239ba52b
Pepe de Sintas Mesquida <josemaria.desintasmesquida@usp.ceu.es>
2024-05-08 20:07:19 +0200

    patientManager UI done

--- FertilityClinic/src/FertilityClinicInterfaces/PatientManager.java
+++ FertilityClinic/src/FertilityClinicInterfaces/PatientManager.java
@@ -5,6 +5,12 @@ public interface PatientManager {
 	public void connect();
 	public void disconnect();
 	public void viewMyInfo();
+	public void manageInfo();
 	public void manageAppointment();
+	public void manageTransactions();
+	public void viewAppointment();
+	public void bookAppointment();
+	public void deleteAppointment();
+	public void modifyAppointment();
 
 }
