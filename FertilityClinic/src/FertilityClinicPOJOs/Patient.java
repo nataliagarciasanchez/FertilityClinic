537793ef8a732b4524d09fccab5a7836b24764b4
nataliagarciasanchez <natalia.garciasanchez@usp.ceu.es>
2024-05-08 13:33:15 +0200

    Cambios Natalia 3

--- FertilityClinic/src/FertilityClinicPOJOs/Patient.java
+++ FertilityClinic/src/FertilityClinicPOJOs/Patient.java
@@ -30,4 +30,90 @@ public class Patient implements Serializable{
 		this.gender = gender;
 	}
 	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDob() {
+		return dob;
+	}
+
+	public void setDob(LocalDate dob) {
+		this.dob = dob;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getPhoneN() {
+		return phoneN;
+	}
+
+	public void setPhoneN(Integer phoneN) {
+		this.phoneN = phoneN;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public BloodType getBloodType() {
+		return bloodType;
+	}
+
+	public void setBloodType(BloodType bloodType) {
+		this.bloodType = bloodType;
+	}
+
+	public Integer getBanckAc() {
+		return banckAc;
+	}
+
+	public void setBanckAc(Integer banckAc) {
+		this.banckAc = banckAc;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
 }
