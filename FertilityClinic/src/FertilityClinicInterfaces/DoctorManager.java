316e611e71e5a8201acd3a14c57c67ecb671f27b
miguelnunez21 <miguel.nunezrobledo@usp.ceu.es>
2024-05-09 18:02:40 +0200

    Merge branch 'master' of https://github.com/nataliagarciasanchez/FertilityClinic.git

---
+++ FertilityClinic/src/FertilityClinicInterfaces/DoctorManager.java
@@ -0,0 +1,23 @@
+package FertilityClinicInterfaces;
+
+import FertilityClinicPOJOs.Doctor;
+
+public class DoctorManager {
+	public void viewStock();
+	public void askForStock();
+	public void viewPatients();
+	public void searchPatientById(Integer Id);
+	public void modifyPatientInfo();
+	public void addPatient ();
+	public void removePatientById (Integer id);
+	public void viewAppointment();
+	public void bookAppointment();
+	public void deleteAppointment();
+	public void modifyAppointment();
+
+	
+	
+	
+	
+
+}
