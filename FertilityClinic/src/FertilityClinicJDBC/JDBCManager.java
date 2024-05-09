cf16825ebda1a8b20d7e9e9201fe1cb271baad38
alexs <alexs@10.60.59.120>
2024-05-09 19:39:59 +0200

    cambios jdbc manager

--- FertilityClinic/src/FertilityClinicJDBC/JDBCManager.java
+++ FertilityClinic/src/FertilityClinicJDBC/JDBCManager.java
@@ -3,6 +3,7 @@ package FertilityClinicJDBC;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCManager {
 	
@@ -17,7 +18,7 @@ private Connection c = null;
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			
 			System.out.print("Database Connection opened.");
-			//this.createTables();
+			this.createTables();
 			
 		}
 		catch(SQLException e) {
@@ -26,6 +27,38 @@ private Connection c = null;
 		catch(ClassNotFoundException e) {
 			System.out.print("Libraries not loaded");
 		}
+	}
+	
+	
+	private void createTables() {
+		try {
+			
+			Statement stmt = c.createStatement();
+			//esto hay que cambiarlo
+			String sql = "CREATE TABLE patients ("
+					+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "name TEXT NOT NULL, phone INTEGER, "
+					+ "bankaccount INTEGER, email TEXT NOT NULL,"
+					+ "dob LOCALDATE, height DOUBLE, weight DOUBLE, "
+					+ "bloodType BLOODTYPE, );";
+			stmt.executeUpdate(sql);
+			
+			sql = "CREATE TABLE doctors ("
+					+ "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+					+ "name TEXT NOT NULL, cured BOOLEAN, "
+					+ "typeOfAnimal TEXT CHECK (typeOfAnimal in (dog, cat, bird, hamster)),"
+					+ "coat TEXT, dob DATE NOT NULL, onwer_id INTEGER REFERENCES owners(id));";
+			stmt.executeUpdate(sql);
+			
+			
+			
+		}catch(SQLException e) {
+			if(!e.getMessage().contains("already exists")) 
+			{
+				e.printStackTrace();
+			}			
+		}
+		
 	}
 	
 	public Connection getConnection(){
