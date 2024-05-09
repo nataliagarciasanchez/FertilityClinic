abfea26f1d9fdf69ef16daefb6fc58d1e7e0564c
miguelnunez21 <miguel.nunezrobledo@usp.ceu.es>
2024-05-09 18:07:27 +0200

    cambios doctormanager interface

--- FertilityClinic/src/FertilityClinicInterfaces/DoctorManager.java
+++ FertilityClinic/src/FertilityClinicInterfaces/DoctorManager.java
@@ -8,13 +8,14 @@ public class DoctorManager {
 	public void viewPatients();
 	public void searchPatientById(Integer Id);
 	public void modifyPatientInfo();
-	public void addPatient ();
+	public void addPatient (Patient patient);
 	public void removePatientById (Integer id);
 	public void viewAppointment();
 	public void bookAppointment();
 	public void deleteAppointment();
 	public void modifyAppointment();
 
+	
 	
 	
 	
