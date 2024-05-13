214b509ac9fb08bd8ce4acc1cdcf9de42db54905
miguelnunez21 <miguel.nunezrobledo@usp.ceu.es>
2024-05-13 12:32:34 +0200

    añadido age

--- FertilityClinic/src/FertilityClinicJDBC/JDBCDoctorManager.java
+++ FertilityClinic/src/FertilityClinicJDBC/JDBCDoctorManager.java
@@ -1,5 +1,27 @@
 package FertilityClinicJDBC;
+import FertilityClinicInterfaces.DoctorManager;
+import FertilityClinicPOJOs.Patient;
+import java.sql.PreparedStatement;
 
 public class JDBCDoctorManager {
+	private JDBCManager manager;
+	
+	public JDBCDoctorManager (JDBCManager manager) {
+		this.manager=manager;
+	}
+	
+	@Override
+	public void addPatient (Patient p) {
+		try {
+			String sql ="INSERT INTO patients (Patient_id, Name, Age, Height, Weight, Phone, Bloodtype, Email, BankAccountNumber, Gender )"
+						+"VALUES(dejar claro los atributos)";
+			PreparedStatement prep=manager.getConnection().prepareStatement(sql);
+			
+			prep.setInt(1, p.getId());
+			prep.setString(2, p.getName());
+			prep.setInt(3, p.getAge);
+		}
+		
+	}
 
 }
