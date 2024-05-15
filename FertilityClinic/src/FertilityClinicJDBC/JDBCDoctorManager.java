6e120e96e94ddb64863b54a1a2fa65bedba4ddcd
nataliagarciasanchez <natalia.garciasanchez@usp.ceu.es>
2024-05-15 22:09:12 +0200

    Cambios

--- FertilityClinic/src/FertilityClinicJDBC/JDBCDoctorManager.java
+++ FertilityClinic/src/FertilityClinicJDBC/JDBCDoctorManager.java
@@ -1,5 +1,6 @@
 package FertilityClinicJDBC;
 import FertilityClinicInterfaces.DoctorManager;
+import FertilityClinicPOJOs.Doctor;
 import FertilityClinicPOJOs.Patient;
 import java.sql.PreparedStatement;
 
@@ -10,6 +11,27 @@ public class JDBCDoctorManager {
 		this.manager=manager;
 	}
 	
+	public void createDoctor(Doctor d) {
+		try {
+			String sql ="INSERT INTO patients (Patient_id, Name, Age, Height, Weight, Phone, Bloodtype, Email, BankAccountNumber, Gender )"
+					+"VALUES(dejar claro los atributos)";
+			PreparedStatement prep=manager.getConnection().prepareStatement(sql);
+		
+			prep.setInt(1, p.getId());
+			prep.setString(2, p.getName());
+			prep.setInt(3, p.getAge());
+			prep.setDouble(4, p.getHeight());
+			prep.setDouble(5, p.getWeight());
+			prep.setInt(6,  p.getPhoneN());
+			prep.setString(7, p.getBloodType());
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/*
 	@Override
 	public void addPatient (Patient p) {
 		try {
@@ -29,5 +51,6 @@ public class JDBCDoctorManager {
 		}
 		
 	}
+	*/
 
 }
