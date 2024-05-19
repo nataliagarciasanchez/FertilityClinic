fb134c9da6314a77ac2485ccddb30ff7d73d32b8
miguelnunez21 <miguel.nunezrobledo@usp.ceu.es>
2024-05-19 16:39:09 +0200

    xmlmamager

--- FertilityClinic/src/FertilityClinicXML/XMLManagerImpl.java
+++ FertilityClinic/src/FertilityClinicXML/XMLManagerImpl.java
@@ -5,6 +5,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
 import FertilityClinicInterfaces.DoctorManager;
 import FertilityClinicInterfaces.PatientManager;
 import FertilityClinicInterfaces.XMLManager;
@@ -12,6 +15,7 @@ import FertilityClinicJDBC.JDBCManager;
 import FertilityClinicJDBC.JDBCDoctorManager;
 import FertilityClinicPOJOs.Doctor;
 import FertilityClinicPOJOs.Patient;
+
 
 public class XMLManagerImpl implements XMLManager {
 	
@@ -27,20 +31,26 @@ public class XMLManagerImpl implements XMLManager {
 		Doctor d = null;
 		List<Patient> patients = new ArrayList<Patient>();
 		manager = new JDBCManager();
-		//doctormanager = new JDBCDoctorManager(manager);
-		//patientmanager = new JDBCPatientManager(manager);
+		doctormanager = new JDBCDoctorManager(manager);//Falta crear el metodo de searchDoctorById(id)
+		patientmanager = new JDBCPatientManager(manager);
 		
 		try {
 			//Do a sql query to get the doctor by id
-			//d = doctormanager.searchDoctorById(id); Falta crear el metodo de searchDoctorById(id)
+			d = doctormanager.searchDoctorById(id); 
+			patients=patientmanager.getListOfPatients();
+			d.setPatients(patients);
 			
-		} catch() {
+			JAXBContext jaxbContext= JAXBContext.newInstance(Doctor.class);
+			Marshaller marshaller=jaxbContext.createMarshaller();
+			File file=new File("./xmls/Doctor.xml");
+			marshaller.marshal(d, file);
+		} catch(Exception e) {
 			
 		}
 	}
 
 	@Override
-	public Patient xml2Pet(File xml) {
+	public Doctor xml2Doctor(File xml) {
 		// TODO Auto-generated method stub
 		return null;
 	}
