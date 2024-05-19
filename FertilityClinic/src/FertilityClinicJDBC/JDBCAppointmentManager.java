219c924c39c77a35f67208841c5cc1c41c018aed
Pepe de Sintas Mesquida <josemaria.desintasmesquida@usp.ceu.es>
2024-05-19 18:13:42 +0200

    j

--- FertilityClinic/src/FertilityClinicJDBC/JDBCAppointmentManager.java
+++ FertilityClinic/src/FertilityClinicJDBC/JDBCAppointmentManager.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Time;
 import java.util.ArrayList;
-
 import FertilityClinicInterfaces.AppointmentManager;
 import FertilityClinicPOJOs.Appointment;
 
